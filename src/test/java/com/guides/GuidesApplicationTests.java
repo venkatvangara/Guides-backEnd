b7546bb23cc3f62b09cd04cc78eafa81a4ca6e49
Vangara Venkat <v.venkat.venkat5@gmail.com>
2026-02-20 21:55:14 +0100

    initial project setup

---
+++ src/test/java/com/guides/GuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
